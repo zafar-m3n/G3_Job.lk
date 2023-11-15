915c908de3eb05ffcae5c2b3560ba7ab92917353
Chanka Herath <cb010779@students.apiit.lk>
2023-11-15 19:58:33 +0530

    Server configurations

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import mysql from "mysql";
+import cors from "cors";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import cookieParser from "cookie-parser";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "freelancer_system",
+});
+
+app.listen(8081, () => {
+  console.log("Running server");
+});
