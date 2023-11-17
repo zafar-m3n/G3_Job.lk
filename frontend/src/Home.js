ef76dc6666d6e5a08e0ab2bdfcdb04e6a70f6602
Chanka Herath <cb010779@students.apiit.lk>
2023-11-18 00:14:04 +0530

    Remaining landing page UI

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -8,6 +8,7 @@ import {
   Col,
   Carousel,
   Card,
+  Form,
 } from "react-bootstrap";
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
 
@@ -31,6 +32,27 @@ function Home() {
       : [arr];
 
   const serviceGroups = chunkServices(services, 4);
+
+  const reviews = [
+    {
+      name: "John Doe",
+      title: "Web Developer",
+      review: "This platform has been instrumental in my career growth.",
+      image: "path-to-profile-picture.jpg",
+    },
+    {
+      name: "Jane Doe",
+      title: "Web Developer",
+      review: "This platform has been instrumental in my career growth.",
+      image: "path-to-profile-picture.jpg",
+    },
+    {
+      name: "Jack Doe",
+      title: "Web Developer",
+      review: "This platform has been instrumental in my career growth.",
+      image: "path-to-profile-picture.jpg",
+    },
+  ];
 
   return (
     <>
@@ -125,7 +147,164 @@ function Home() {
         </Carousel>
       </Container>
 
-      <Container fluid className="my-5 px-5 third-section"></Container>
+      <Container fluid className="my-5 p-5 third-section">
+        <Row className="justify-content-between m-0 list-section">
+          <Col md={5} className="p-0 list-section align-items-center">
+            <h1 className="mb-4 heading">The best part? Everything.</h1>
+            <ul className="list-unstyled bg-transparent list-section">
+              <li className="mb-2 list-section">
+                <i class="fa-regular fa-circle-check list-section me-2"></i>{" "}
+                Stick to your budget
+                <p className="list-section">
+                  Find the right service for every price point. No hourly rates,
+                  just project-based pricing.
+                </p>
+              </li>
+              <li className="mb-2 list-section">
+                <i class="fa-regular fa-circle-check list-section me-2"></i> Get
+                quality work done quickly
+                <p className="list-section">
+                  Hand your project over to a talented freelancer in minutes,
+                  get long-lasting results.
+                </p>
+              </li>
+              <li className="mb-2 list-section">
+                <i class="fa-regular fa-circle-check list-section me-2"></i> Pay
+                when you're happy
+                <p className="list-section">
+                  Upfront quotes mean no surprises. Payments only get released
+                  when you approve.
+                </p>
+              </li>
+              <li className="mb-2 list-section">
+                <i class="fa-regular fa-circle-check list-section me-2"></i>
+                Count on 24/7 support
+                <p className="list-section">
+                  Our round-the-clock support team is available to help anytime,
+                  anywhere.
+                </p>
+              </li>
+            </ul>
+          </Col>
+
+          <Col md={6} className="p-0 bg-transparent d-flex justify-content-end">
+            <img
+              src="corporate-workers.jpg"
+              alt="Corporate Workers"
+              className="img-fluid"
+            />
+          </Col>
+        </Row>
+      </Container>
+
+      <Container fluid className="my-5 px-5 fourth-section">
+        <h1 className="mb-4 heading text-center">What our customers say</h1>
+        <Row>
+          <Col md={6} className="mx-auto">
+            <Carousel
+              indicators={false}
+              interval={null}
+              prevIcon={
+                <span aria-hidden="true" className="carousel-control-prev-icon">
+                  <i className="fas fa-arrow-circle-left fa-2x"></i>
+                </span>
+              }
+              nextIcon={
+                <span aria-hidden="true" className="carousel-control-next-icon">
+                  <i className="fas fa-arrow-circle-right fa-2x"></i>
+                </span>
+              }
+              className="custom-carousel"
+            >
+              {reviews.map((review, index) => (
+                <Carousel.Item>
+                  <Row>
+                    <Col key={index} className="d-flex justify-content-center">
+                      <Card
+                        style={{ width: "500px", height: "300px" }}
+                        className="text-center"
+                      >
+                        <Card.Img
+                          variant="top"
+                          src={review.image}
+                          className="rounded-circle mx-auto"
+                          style={{ width: "100px", height: "100px" }}
+                        />
+                        <Card.Body>
+                          <Card.Title>{review.name}</Card.Title>
+                          <Card.Subtitle className="mb-2">
+                            {review.title}
+                          </Card.Subtitle>
+                          <Card.Text>{review.review}</Card.Text>
+                        </Card.Body>
+                      </Card>
+                    </Col>
+                  </Row>
+                </Carousel.Item>
+              ))}
+            </Carousel>
+          </Col>
+        </Row>
+      </Container>
+
+      <Container fluid className="mt-5 px-5 pt-3 fifth-section">
+        <Row className="bg-transparent">
+          {/* First Column - Logo and About */}
+          <Col md={5} className="mx-auto mb-3 bg-transparent">
+            <Navbar.Brand href="/" className="fs-3 footer-logo bg-transparent">
+              Job.lk
+            </Navbar.Brand>
+            <p className="footer-content">
+              Job.lk is a specialized freelance platform tailored for web
+              developer freelancers in Sri Lanka. Our platform serves as a
+              dynamic marketplace, connecting talented web developers with a
+              diverse range of opportunities. Whether you are a skilled web
+              developer seeking exciting projects or an employer searching for
+              top-notch web development expertise, Job.lk is your go-to
+              destination. Discover the perfect match for your project or
+              showcase your skills to a local and global audience, all on
+              Job.lk, the premier choice for web development freelancers in Sri
+              Lanka.
+            </p>
+          </Col>
+
+          {/* Second Column - Quick Links */}
+          <Col md={3} className="mx-auto mb-3 bg-transparent">
+            <h5 className="footer-content">Quick Links</h5>
+            <Nav className="footer-content">
+              <Col className="footer-content">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="#jobs">Jobs</Nav.Link>
+                <Nav.Link href="#freelancers">Freelancers</Nav.Link>
+                <Nav.Link href="#how-it-works">How it works?</Nav.Link>
+              </Col>
+            </Nav>
+          </Col>
+
+          {/* Third Column - Contact / Subscription Form */}
+          <Col md={3} className="mx-auto mb-3 bg-transparent">
+            <h5 className="footer-content">Get in touch</h5>
+            <Form className="footer-content">
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Control type="email" placeholder="Enter email" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicName">
+                <Form.Control type="text" placeholder="Your Name" />
+              </Form.Group>
+              <Button
+                variant="primary"
+                type="submit"
+                className="custom-primary-btn"
+              >
+                Subscribe
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+        
+            <h5 className="footer-content text-center">CCG3 All Rights Reserved.</h5>
+          
+      </Container>
     </>
   );
 }
